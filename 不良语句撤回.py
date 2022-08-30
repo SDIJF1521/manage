979b0259376e8302eb8a0134edb545a42b9bb3a0
YGXB <69745333+SDIJF1521@users.noreply.github.com>
2022-08-30 10:29:33 +0800

    Update 不良语句撤回.py

--- 不良语句撤回.py
+++ 不良语句撤回.py
@@ -26,7 +26,7 @@ async def forbid_word(event1:GroupMessageEvent,event:Event):
         userID.append(str(library[i][0]))
     user_id = event.get_user_id()
     user = event1.group_id
-    if user_id in userID and user == 566077032:
+    if user_id in userID and user == 群号:
         cursor.execute('select wordlist from word')
         blacklist_word = cursor.fetchall()
         blacklist_word_list = []
@@ -68,7 +68,7 @@ async def dl(event1:GroupMessageEvent,event:Event):
         userID.append(str(library[i][0]))
     user_id = event.get_user_id()
     user = event1.group_id
-    if user_id in userID and user == 566077032:
+    if user_id in userID and user ==  群号:
         cursor.execute('select wordlist from word')
         blacklist_word = cursor.fetchall()
         blacklist_word_list = []
@@ -100,27 +100,29 @@ async def dl(event1:GroupMessageEvent,event:Event):
 
 @withdraw.handle()
 async def WM(event:GroupMessageEvent,bot:Bot):
-    cursor.execute('select wordlist from word')
-    word = cursor.fetchall()
-    word_list = []
-    for i in range(len(word)):
-        word_list.append(word[i][0])
-    cursor.execute('select admin from gl')
-    library = cursor.fetchall()
-    userID = []
-    for i in range(len(library)):
-        userID.append(str(library[i][0]))
-    for i in range(len(userID)):
-        userID[i] = str(userID[i])
-    if not  str(event.get_user_id) in userID:
-        print(userID, '\n' + str(event.get_user_id))
-        Data1 = re.compile('.+|').findall(event.get_event_description())
-        Data2 = ''
-        for i in Data1:
-            Data2= Data2+i
-        for j in word_list:
-            Data = re.compile(j).search(re.compile('] "(.+)').search(Data2).group().replace(' ',''))
-            print(Data2 ,'\n'+re.compile('] "(.+)').search(Data2).group().replace(' ',''),'\n',Data)
-            if Data != None:
-                await bot.delete_msg(message_id=event.message_id)
-                break
+    user = event.group_id
+    if user ==  群号:
+        cursor.execute('select wordlist from word')
+        word = cursor.fetchall()
+        word_list = []
+        for i in range(len(word)):
+            word_list.append(word[i][0])
+        cursor.execute('select admin from gl')
+        library = cursor.fetchall()
+        userID = []
+        for i in range(len(library)):
+            userID.append(str(library[i][0]))
+        for i in range(len(userID)):
+            userID[i] = str(userID[i])
+        if not  str(event.get_user_id) in userID:
+            print(userID, '\n' + str(event.get_user_id))
+            Data1 = re.compile('.+|').findall(event.get_event_description())
+            Data2 = ''
+            for i in Data1:
+                Data2= Data2+i
+            for j in word_list:
+                Data = re.compile(j).search(re.compile('] "(.+)').search(Data2).group().replace(' ',''))
+                print(Data2 ,'\n'+re.compile('] "(.+)').search(Data2).group().replace(' ',''),'\n',Data)
+                if Data != None:
+                    await bot.delete_msg(message_id=event.message_id)
+                    break
